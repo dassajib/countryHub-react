a0d57a9b8ff2bf0828cbe2dcb058497b4e12db12
dassajib <sajibdas.sd@gmail.com>
2023-09-19 21:41:07 +0600

    useparam functionility and header added

--- src/Index/Index.jsx
+++ src/Index/Index.jsx
@@ -3,10 +3,12 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../Components/Home/Home";
 import CountryDetails from "../Components/CountryDetails/CountryDetails";
 import AllCountry from "../Components/AllCountry/AllCountry";
+import Header from "../Components/Header/Header";
 
 const Index = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
